e32575cb7db95eab9aa546112be87444d8cc3784
AhmedEl-Metwally <ahmed.moh.elmetwally@gmail.com>
2025-09-21 17:20:29 +0300

    Department  Create

--- Company.BLL/Dtos/CreateDepartmentDto.cs
+++ Company.BLL/Dtos/CreateDepartmentDto.cs
@@ -1,10 +1,12 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Company.BLL.Dtos
 {
     public class CreateDepartmentDto
     {
+        [Required]
         public string Name { get; set; } = string.Empty;
+        [Required]
         public string Code { get; set; } = string.Empty;
         public string? Description { get; set; }
         public DateOnly DateOfCreation { get; set; }
--- Company.PL/Controllers/DepartmentController.cs
+++ Company.PL/Controllers/DepartmentController.cs
@@ -1,10 +1,17 @@
-﻿using Company.BLL.Repositories.Interface;
+﻿using Company.BLL.Dtos;
+using Company.BLL.Repositories.Interface;
 using Company.BLL.Repositories.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Company.PL.Controllers
 {
-    public class DepartmentController(IDepartmentService _departmentService) : Controller
+    public class DepartmentController
+                                    (
+                                    IDepartmentService _departmentService,
+                                    IWebHostEnvironment _webHostEnvironment,
+                                    ILogger<DepartmentController> _logger
+                                    ) : Controller
+
     {
         //Index
         [HttpGet]
@@ -13,5 +20,53 @@ namespace Company.PL.Controllers
             var department = _departmentService.GetAllDepartments(); 
             return View(department);
         }
+
+
+        //Create
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateDepartmentDto createDepartmentDto)
+        {
+            if(ModelState.IsValid)
+            {
+                try
+                {
+                    int result = _departmentService.AddDepartment(createDepartmentDto);
+                    if (result > 0)
+                        return RedirectToAction(nameof(Index));
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Depatment can not created");
+                    }
+                }
+                catch (Exception ex) 
+                {
+                    //Development ==> action , log error in console , view
+                    if(_webHostEnvironment.IsDevelopment())
+                    {
+                        _logger.LogError($"Department can not be create because : {ex.Message}");
+                    }
+                    else
+                    {
+                        _logger.LogError($"\"Department can not be create because : {ex}");
+                        return View("ErrorView");
+                    }
+                }
+               
+            }
+             return View(createDepartmentDto);
+        }
+        
+        
+
+     
+
+
+
     }
 }
