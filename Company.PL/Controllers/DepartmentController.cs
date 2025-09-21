57bbdd293f594d4a846d113bfaf6dd1bc543c527
AhmedEl-Metwally <ahmed.moh.elmetwally@gmail.com>
2025-09-21 18:08:47 +0300

    Department  Details

--- Company.PL/Controllers/DepartmentController.cs
+++ Company.PL/Controllers/DepartmentController.cs
@@ -61,10 +61,21 @@ namespace Company.PL.Controllers
             }
              return View(createDepartmentDto);
         }
-        
-        
 
-     
+        //Details
+        [HttpGet]
+        public IActionResult Details(int? id)
+        {
+            if(!id.HasValue)
+                return BadRequest();
+            var department = _departmentService.GetDepartmentById(id.Value);
+            if(department is null)
+                return BadRequest();
+
+            return View(department);
+        }
+
+
 
 
 
