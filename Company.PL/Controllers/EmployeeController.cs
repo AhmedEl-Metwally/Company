5da3ff9ac2b943bad7886b5f9de06024a0310256
AhmedEl-Metwally <ahmed.moh.elmetwally@gmail.com>
2025-09-23 18:19:09 +0300

    Employee  Create

--- Company.PL/Controllers/EmployeeController.cs
+++ Company.PL/Controllers/EmployeeController.cs
@@ -1,15 +1,61 @@
-﻿using Company.BLL.Services.Interface;
+﻿using Company.BLL.Dtos.DepartmentDtos;
+using Company.BLL.Dtos.EmployeeDtos;
+using Company.BLL.Services.Implementation;
+using Company.BLL.Services.Interface;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Company.PL.Controllers
 {
-    public class EmployeeController(IEmployeeService _employeeService) : Controller
+    public class EmployeeController
+                                  (
+                                   IEmployeeService _employeeService,
+                                   IWebHostEnvironment _webHostEnvironment,
+                                   ILogger<EmployeeController> _logger
+                                  ) : Controller
     {
         [HttpGet]
         public IActionResult Index()
         {
             var employees = _employeeService.GetAllEmployee();
             return View(employees);
+        }
+
+        //Create
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreatedEmployeeDto createdEmployeeDto)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    int result = _employeeService.CreateEmployee(createdEmployeeDto);
+                    if (result > 0)
+                        return RedirectToAction(nameof(Index));
+                    else
+                        ModelState.AddModelError(string.Empty, "CreatedEmployee can not created");
+                }
+                catch (Exception ex)
+                {
+                    //Development ==> action , log error in console , view
+                    if (_webHostEnvironment.IsDevelopment())
+                        _logger.LogError($"CreatedEmployee can not be create because : {ex.Message}");
+                    else
+                    {
+                        _logger.LogError($"\"CreatedEmployee can not be create because : {ex}");
+                        return View("ErrorView");
+                    }
+                }
+
+            }
+            return View(createdEmployeeDto);
         }
     }
 }
