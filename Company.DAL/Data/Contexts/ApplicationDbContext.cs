ad740c604c1bdd6fca4ce566506e5b9d98e49226
AhmedEl-Metwally <ahmed.moh.elmetwally@gmail.com>
2025-09-25 21:13:09 +0300

    IdentityUser

--- Company.DAL/Data/Contexts/ApplicationDbContext.cs
+++ Company.DAL/Data/Contexts/ApplicationDbContext.cs
@@ -1,16 +1,21 @@
 ﻿using Company.DAL.Models.DepartmentModule;
 using Company.DAL.Models.EmployeeModule;
+using Company.DAL.Models.IdentityModels;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace Company.DAL.Data.Contexts
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUsers>(options)
     {
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        protected override void OnModelCreating(ModelBuilder modelBuilder) 
         {
+            base.OnModelCreating(modelBuilder);
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
         }
 
         public DbSet<Department> Departments { get; set; }
         public DbSet<Employee> Employees { get; set; }
+        //public DbSet<IdentityUser> IdentityUsers  { get; set; }
+        //public DbSet<IdentityRole> IdentityRoles { get; set; }
     }
 }
