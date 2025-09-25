fad310d3be479c30b7fa2314c6e8c896f3e9676d
AhmedEl-Metwally <ahmed.moh.elmetwally@gmail.com>
2025-09-25 18:18:16 +0300

    Unit of Work With Lazy Implementation

--- Company.DAL/Repositories/UnitOfWorks/UnitOfWork.cs
+++ Company.DAL/Repositories/UnitOfWorks/UnitOfWork.cs
@@ -1,15 +1,27 @@
 ﻿using Company.DAL.Data.Contexts;
+using Company.DAL.Repositories.Implementation;
 using Company.DAL.Repositories.Interface;
 
 
 
 namespace Company.DAL.Repositories.UnitOfWorks
 {
-    public class UnitOfWork(IDepartmentRepositories _departmentRepositories, IEmployeeRepositories _employeeRepositories,ApplicationDbContext _context) : IUnitOfWork
+    public class UnitOfWork : IUnitOfWork
     {
-        public IEmployeeRepositories EmployeeRepositories => _employeeRepositories;
+        private readonly ApplicationDbContext _context;
+        private readonly Lazy<IEmployeeRepositories> _employeeRepositories;
+        private readonly Lazy<IDepartmentRepositories> _departmentRepositories;
 
-        public IDepartmentRepositories DepartmentRepositories => _departmentRepositories;
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+            _employeeRepositories = new Lazy<IEmployeeRepositories>(()=> new EmployeeRepositories(_context));
+            _departmentRepositories = new Lazy<IDepartmentRepositories>(()=> new DepartmentRepositories(_context));
+        }
+
+        public IEmployeeRepositories EmployeeRepositories => _employeeRepositories.Value;
+
+        public IDepartmentRepositories DepartmentRepositories => _departmentRepositories.Value;
 
         public int SaveChanges() => _context.SaveChanges();
       
