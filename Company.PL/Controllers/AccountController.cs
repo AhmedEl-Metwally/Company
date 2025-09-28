0c546989bc4dea6df31d2a6476d4edd6b8660968
AhmedEl-Metwally <ahmed.moh.elmetwally@gmail.com>
2025-09-28 21:17:52 +0300

    AuthLayout & Register

---
+++ Company.PL/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using Company.DAL.Models.IdentityModels;
+using Company.PL.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Company.PL.Controllers
+{
+    public class AccountController(UserManager<ApplicationUsers> _userManager) : Controller
+    {
+        //Register
+        [HttpGet]
+        public IActionResult Register() => View();
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(RegisterViewModel registerViewModel)
+        {
+            if (!ModelState.IsValid) 
+                return View(registerViewModel);
+
+            var user = new ApplicationUsers
+            {
+                FirstName = registerViewModel.FirstName,
+                LastName = registerViewModel.LastName,
+                UserName = registerViewModel.Username,
+                Email = registerViewModel.Email,
+            };
+
+            var result = _userManager.CreateAsync(user, registerViewModel.Password).Result;
+            if (result.Succeeded)
+                return RedirectToAction("Login");
+            else
+            {
+                foreach (var error in result.Errors)
+                    ModelState.AddModelError(string.Empty, error.Description);
+                return View(registerViewModel);
+            }
+        }
+
+
+
+
+
+        //SignIn
+        //SignOut
+    }
+}
--- Company.PL/Program.cs
+++ Company.PL/Program.cs
@@ -2,11 +2,14 @@ using Company.BLL.Mapping;
 using Company.BLL.Services.Implementation;
 using Company.BLL.Services.Interface;
 using Company.DAL.Data.Contexts;
+using Company.DAL.Models.IdentityModels;
 using Company.DAL.Repositories.Implementation;
 using Company.DAL.Repositories.Interface;
 using Company.DAL.Repositories.UnitOfWorks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,6 +28,7 @@ builder.Services.AddScoped<IDepartmentService, DepartmentService>();
 builder.Services.AddScoped<IEmployeeRepositories, EmployeeRepositories>();
 builder.Services.AddScoped<IEmployeeService, EmployeeService>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddIdentity<ApplicationUsers,IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddAutoMapper(Mapping => Mapping.AddProfile(new MappingProfile()));
 
 
@@ -47,7 +51,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Account}/{action=Register}/{id?}")
     .WithStaticAssets();
 
 
